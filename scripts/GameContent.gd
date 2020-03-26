e5ca4fa4f2ee28fc52639cbe760673baf1142e5e
John Gabriel <contact@johngabriel.co.uk>
2020-03-26 21:01:07 +0000

    Let game fall back on loading maps/palettes from within pck if necessary

--- scripts/GameContent.gd
+++ scripts/GameContent.gd
@@ -1,9 +1,8 @@
 extends Node
 
 const ORIGINAL_MAP_PATH : String = "res://maps"
-const CUSTOM_MAP_PATH : String = "user://maps"
-
 const ORIGINAL_PALETTE_PATH : String = "res://textures/palettes"
+const CUSTOM_MAP_PATH : String = "user://maps"
 const CUSTOM_PALETTE_PATH : String = "user://palettes"
 
 const MAP_FILES : Array = [
@@ -20,21 +19,47 @@ const PALETTE_FILES : Array = [
 	"cold_blue.png"
 ]
 
+const ORIGINAL_MAPS : Array = [
+	preload("res://maps/level1.png"),
+	preload("res://maps/level2.png"),
+	preload("res://maps/level3.png"),
+	preload("res://maps/level4.png")
+]
+
+const ORIGINAL_PALETTES : Array = [
+	preload("res://textures/palettes/slso8.png"),
+	preload("res://textures/palettes/title.png"),
+	preload("res://textures/palettes/hot.png"),
+	preload("res://textures/palettes/cold_blue.png")
+]
+
 var maps : Array
 var palettes : Array
 
-func load_map(path : String) -> Image:
-	var image : ImageTexture = ImageTexture.new()
-	image.load(path)
-	var map : Image = image.get_data()
-	map.lock()
-	return map
+func load_map(which : int) -> Image:
+	var file : File = File.new()
+	var path : String = CUSTOM_MAP_PATH + "/" + MAP_FILES[which]
+	if file.file_exists(path):
+		var image : ImageTexture = ImageTexture.new()
+		image.load(path)
+		var map : Image = image.get_data()
+		map.lock()
+		return map
+	else:
+		var map : Image = ORIGINAL_MAPS[which].get_data()
+		map.lock()
+		return map
 
-func load_palette(path : String) -> ImageTexture:
-	var image : ImageTexture = ImageTexture.new()
-	image.load(path)
-	image.flags = image.FLAG_FILTER
-	return image
+func load_palette(which : int) -> Texture:
+	var file : File = File.new()
+	var path : String = CUSTOM_PALETTE_PATH + "/" + PALETTE_FILES[which]
+	if file.file_exists(path):
+		var image : Texture = ImageTexture.new()
+		image.load(path)
+		image.flags = image.FLAG_FILTER
+		return image
+	else:
+		return ORIGINAL_PALETTES[which]
 
 func get_map(which : int) -> Image:
 	return maps[which]
@@ -44,24 +69,11 @@ func get_palette(which : int) -> Image:
 
 func load_content() -> void:
 	maps = [null, null, null, null]
-	for i in range(0, MAP_FILES.size()):
-		maps[i] = load_map(CUSTOM_MAP_PATH + "/" + MAP_FILES[i])
 	palettes = [null, null, null, null]
+	for i in range(0, MAP_FILES.size()):
+		maps[i] = load_map(i)
 	for i in range(0, PALETTE_FILES.size()):
-		palettes[i] = load_palette(CUSTOM_PALETTE_PATH + "/" + PALETTE_FILES[i])
-
-# During first time installation, content is loaded into user directory for easier modification
-func extract_content() -> void:
-	var d : Directory = Directory.new()
-	if not d.dir_exists(CUSTOM_MAP_PATH):
-		d.make_dir(CUSTOM_MAP_PATH)
-		for map in MAP_FILES:
-			d.copy(ORIGINAL_MAP_PATH + "/" + map, CUSTOM_MAP_PATH + "/" + map)
-	if not d.dir_exists(CUSTOM_PALETTE_PATH):
-		d.make_dir(CUSTOM_PALETTE_PATH)
-		for palette in PALETTE_FILES:
-			d.copy(ORIGINAL_PALETTE_PATH + "/" + palette, CUSTOM_PALETTE_PATH + "/" + palette)
+		palettes[i] = load_palette(i)
 
 func _ready() -> void:
-	extract_content()
 	load_content()
